8e3c231be2ae0fef7f9f027fc81280b8cf347df6
Sumit Kumar <sumit.sh25@yahoo.com>
2019-10-23 05:03:19 +0530

    checkin 23Oct Main sec dev

---
+++ src/App.vue
@@ -0,0 +1,188 @@
+<template>
+  <div id="app">
+    <header id="nav">
+      <div class="brand">
+        <div class="brand_logo">
+          <img src="@/assets/sklogo.png" alt srcset height="25px" />
+        </div>
+        <div class="brand_text">toosumit</div>
+      </div>
+      <div class="ham_icon" @click="showMenu">
+        <font-awesome-icon :icon="['fas', 'bars']" />
+      </div>
+      <div class="nav_menu">
+        <div class="menu_item">Home</div>
+        <div class="menu_item">Projects</div>
+        <div class="menu_item">Misc</div>
+        <div class="menu_item">Contact</div>
+      </div>
+    </header>
+    <router-view />
+    <div class="footer">
+      <div class="brand">
+        <div class="brand_logo">
+          <img src="@/assets/sklogo.png" alt srcset height="25px" />
+        </div>
+        <div class="brand_text">toosumit</div>
+      </div>
+      <div class="social_icons">
+        <a href="http://github.com/spark25" target="_blank">
+          <font-awesome-icon :icon="['fab', 'github']" />
+        </a>
+        <a href="https://www.linkedin.com/in/isumitk/" target="_blank">
+          <font-awesome-icon :icon="['fab', 'linkedin']" />
+        </a>
+        <a href="http://instagram.com/toosumit" target="_blank">
+          <font-awesome-icon :icon="['fab', 'instagram']" />
+        </a>
+      </div>
+      <div class="_cp">Designed and developed by Sumit Kumar</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    showMenu() {
+      document.querySelector(".nav_menu").classList.toggle("showMe");
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+// @import "./assets/style/global";
+
+#nav {
+  min-height: 70px;
+  background: $darkSec;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  position: relative;
+  font-family: 'Poppins', sans-serif;
+
+  .ham_icon {
+    font-size: 18px;
+    color: #ddd;
+    display: none;
+
+    @include sm {
+      display: block;
+      cursor: pointer;
+    }
+  }
+  .nav_menu {
+    display: flex;
+    color: #ddd;
+    font-weight: 500;
+    text-transform: lowercase;
+    align-items: center;
+    justify-content: center;
+
+    @include sm {
+      transform: translateX(100%);
+      background: $darkSec;
+      width: 100%;
+      height: 50px;
+      position: absolute;
+      left: 0;
+      bottom: -50px;
+      transition: all 0.3s ease-in;
+    }
+
+    .menu_item {
+      padding: 5px;
+      margin-right: 10px;
+      display: block;
+      height: 100%;
+      position: relative;
+
+      &::after {
+        content: "";
+        width: 100%;
+        height: 3px;
+        background: $yellow;
+        position: absolute;
+        left: 0;
+        bottom: 0;
+        transform: scaleX(0);
+        transform-origin: center;
+        transition: all 0.3s ease-in;
+      }
+
+      &:hover::after {
+        transform: scaleX(1);
+        transform-origin: center;
+        background: $orange;
+        // width: 100%;
+      }
+    }
+  }
+}
+
+.showMe {
+  transform: translateX(0) !important;
+}
+
+.brand {
+  padding: 5px 20px;
+  display: flex;
+  align-items: center;
+
+  .brand_logo {
+    padding-right: 10px;
+  }
+  .brand_text {
+    font-family: "Source Code Pro", monospace;
+    font-weight: 600;
+    font-size: 24px;
+    letter-spacing: 5px;
+    color: #ddd;
+  }
+}
+
+.footer {
+  // font-size: 12px;
+  color: #ddd;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  padding: 3rem;
+  background: $notBlack;
+
+  @include sm {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+  }
+
+  .social_icons {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    grid-gap: 20px;
+    font-size: 1.5rem;
+
+    @include sm {
+    margin-top: 1rem;
+    margin-bottom: 1rem;
+    } 
+
+    a {
+      text-decoration: none;
+      color: #ccc;
+
+      &:hover {
+        color: $offWhite;
+      }
+    }
+  }
+
+  ._cp {
+    font-size: 14px;
+    color: #ccc;
+  }
+}
+</style>
+
---
+++ src/views/Home.vue
@@ -0,0 +1,159 @@
+<template>
+  <main>
+      <div class="main">
+    <div class="left_main">
+      <div class="overlay_grid">
+        <div class="_bg  animated fadeInLeft slow"></div>
+
+      
+        <div class="_fg animated fadeInDown slow">
+         <!-- <div class="content"> -->
+       
+            <div class="title">
+              Fullstack <br>Developer
+              <span style="color:#f78608"></span>  
+            </div>
+            <div class="heading">
+               with love for <br> Design
+            </div>
+              
+         <!-- </div> -->
+            
+        </div>
+        
+      </div>
+    </div>
+    <div class="right_main">
+      <div class="dp_grid">
+         <div class="_frame heading  animated fadeInRight" style="color:#ddd; display:flex; align-items:center; justify-content:center;">Hi,</div>
+        <img src="@/assets/me.png" alt="" srcset="" height="300px" class="_dp animated zoomIn">
+       
+      </div>
+
+      <div class="_intro animated fadeInUp slower">
+        <div class="_intro_text">
+            I am Sumit, a Software Engineer from Pune, India, <br> currently working with Zensar Technologies <br>
+        I create scalable web apps and make sure they look awesome while I am at it.
+        </div>
+      
+        <div class="btn btn-yellow">
+          <div class="btn_text">
+              Know More  
+          </div>
+          <div class="btn_icon">
+            <font-awesome-icon :icon="['fas', 'arrow-right']" />
+
+          </div>
+      
+        
+      </div>
+      </div>
+      
+    </div>
+  </div>
+
+  <section class="section">
+
+  </section>
+  </main>
+
+</template>
+
+<script>
+export default {
+  name: "home",
+  components: {}
+};
+</script>
+<style lang="scss" scoped>
+
+.main {
+  width: 100%;
+  height: 90vh;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  align-items: center;
+
+  @include sm{
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    margin-top: 50px;
+    min-height: 100vh;
+  }
+
+  .left_main {
+    flex: 1;
+    color: #eee;
+    .overlay_grid {
+      display: grid;     
+      grid-template-columns: 50px 50px 5fr 50px 50px;
+      grid-template-rows: 1fr 1fr 50px;
+      // height: 70vh;
+      ._bg {
+        background: transparent;
+        border-top:  5px solid $yellow;
+        border-right:  5px solid $yellow;
+        border-bottom:  5px solid $yellow;
+        grid-column: 1/span 4;
+        grid-row: 2/span 2;
+      }
+      ._fg {
+        //  background: $darkPrime;
+        grid-column: 3/span 1;
+        grid-row: 1/span 2;
+   
+        
+      }
+    }
+  }
+  .right_main {
+    flex: 1;
+    .content{
+      color: #ddd;
+    }
+
+    ._intro{
+      color: #ddd;
+      font-size: 18px;
+      max-width: 500px;
+     
+      ._intro_text{
+         padding-top: 2rem;
+         padding-bottom: 2rem;
+      word-wrap: break-word;
+
+      }
+      
+    }
+  }
+}
+
+.section{
+  background: $offWhite;
+  height: 100vh;
+}
+
+.dp_grid{
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: 100px 200px;
+
+  ._frame{
+      
+        border-top:  10px solid $orange;
+        border-left:  10px solid $orange;
+        border-bottom:  10px solid $orange;
+        background: transparent;
+        grid-row: 2/span 1;
+        grid-column: 1 / span 1;
+  }
+  ._dp{
+       grid-row: 1/span 3;
+      grid-column: 1 / span 3;
+      padding: 10px;
+      z-index: 1;
+  }
+}
+
+</style>
